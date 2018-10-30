2b9d73d84bc7719cce44a48bd14d68eafd6686e8
AceVentura <alexpapinutti@gmail.com>
2018-10-30 16:40:44 +0100

    Style-less web-page

--- main.js
+++ main.js
@@ -2,69 +2,56 @@ var app = new Vue({
     el: "#app",
     data: {
         allInfo: [],
-        search:'',
-        city:"London",
-        country:'uk',
-        url:"https://api.openweathermap.org/data/2.5/weather?q=London",
+        search: '',
+        temperature: 0,
+        minTemp: 0,
+        maxTemp: 0,
+        windSpeed: 0,
+        pressure: 0,
+        city:'',
+        myResponse: [],
     },
-    created() {
-        this.dataTable(this.url);
 
-    },
-    
     methods: {
 
-        dataTable: function (url) {
+        dataTable: function () {
             var fetchConfig =
-                fetch(url, {
+                fetch("https://api.openweathermap.org/data/2.5/weather?q=" + this.search + "&APPID=9f028a252c9482c30904dbda5c75f15b", {
                     method: "GET",
-
-                    headers: ({
-                        "APPID": '9f028a252c9482c30904dbda5c75f15b',
-                        "Access-Control-Allow-Origin":'http://127.0.0.1:52880/',
-                    })
 
                 })
                 .then(function (response) {
                     return response.json();
-                    
+
                 })
-                .then(function (data) {
-                    console.log(data)
+                .then(function (response) {
+                    console.log(response)
+                    app.updateSearch(response);
                     
+//                    console.log(app.myResponse);
                 })
                 .catch(error => alert(error));
         },
-    }
-});
 
-//      allMembers: [],
-//        showFullTable: true,
-//        showPartyTable: false,
-//        showStatesTable: false,
-//        partyArray: [],
-//        statesArray: [],
-//        newStatesArray: [],
-//        senate: "https://api.propublica.org/congress/v1/113/senate/members.json",
-//        house: "https://api.propublica.org/congress/v1/113/house/members.json",
-//    },
-//    created() {
-//        if (location.pathname == "/house-data.html") {
-//            this.dataTable(this.house);
-//        } else if (location.pathname == "/senate-data.html") {
-//            this.dataTable(this.senate);
-//
-//        }
-//    },
-//    methods: {
-//
-//        dataTable: function (link) {
-//            var fetchConfig =
-//                fetch(link, {
-//                    method: "GET",
-//
-//                    headers: ({
-//                        "X-API-Key": '7YtBT0L3pw0Tqffy6BrgOq44kF4guyNotpUJppRr'
-//                    })
-//
-//                })
+        updateSearch: function (response) {
+            let temperature = response.main.temp;
+            let newTemperature = temperature-273.15;
+            app.temperature = newTemperature.toFixed();
+            let minimumTemp = response.main.temp_min;
+            let newMinTemp = minimumTemp-273.15;
+            app.minTemp = newMinTemp.toFixed();
+            let maximumTemp = response.main.temp_max;
+            let newMaxTemp = maximumTemp-273.15;
+            app.maxTemp = newMaxTemp.toFixed();
+            let pressure = response.main.pressure;
+            let newPressure = pressure/1000;
+            app.pressure = newPressure;
+            app.myResponse = response; 
+            app.city = response.name;
+            app.allInfo= response.main;
+            app.windSpeed= response.wind.speed;
+            
+        },
+    }
+
+});
